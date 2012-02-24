ca0bee27e7d3779e2f68f374ea9a8523afc0a321
Ben Ratzlaff <ben.ratzlaff@gmail.com>
2012-02-24 06:53:56 +0000

    Whitespace
    git-svn-id: https://svn.code.sf.net/p/xcmapedit/code/trunk/dev@27 8cc3ff80-43d4-47b0-9ae9-63cba5b67482

--- MapView/Globals.cs
+++ MapView/Globals.cs
@@ -2,30 +2,24 @@ using System;
 using XCom;
 
 namespace MapView
-{	
+{
 	public class Globals
 	{
-		public static int PckImageScale=1;
+		public static int PckImageScale = 1;
 
 		public static bool MapChanged = false;
-		public static bool UseGray{get{return true;}}
+		public static bool UseGray { get { return true; } }
 		public static readonly string RegistryKey = "MapView";
 
-		private static XCom.PckFile extraTiles = null;
-
-		public static XCom.PckFile ExtraTiles
-		{
-			get { return extraTiles; }
-		}
+		public static XCom.PckFile ExtraTiles { get; }
 
 		public static void LoadExtras()
 		{
-			if (extraTiles == null)
-			{
+			if (ExtraTiles == null) {
 				System.IO.Stream sPck = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("MapView._Embedded.Extra.PCK");
 				System.IO.Stream sTab = System.Reflection.Assembly.GetExecutingAssembly().GetManifestResourceStream("MapView._Embedded.Extra.TAB");
 
-				extraTiles = new XCom.PckFile(sPck, sTab, 2, XCPalette.TFTDBattle);
+				ExtraTiles = new XCom.PckFile(sPck, sTab, 2, XCPalette.TFTDBattle);
 			}
 		}
 	}
