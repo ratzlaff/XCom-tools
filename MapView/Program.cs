bc996676c545fdcccce6e928a0681b4e2f5ea5d2
Ben Ratzlaff <ben.ratzlaff@gmail.com>
2012-02-24 06:54:39 +0000

    Remove commented out code related to creating new appDomains
    git-svn-id: https://svn.code.sf.net/p/xcmapedit/code/trunk/dev@28 8cc3ff80-43d4-47b0-9ae9-63cba5b67482

--- MapView/Program.cs
+++ MapView/Program.cs
@@ -6,69 +6,14 @@ using System.Threading;
 
 namespace MapView
 {
-	static class Program
+	public class Program : Startup<MainWindow>
 	{
 		[STAThread]
 		public static void Main()
 		{
-			// Construct and initialize settings for a second AppDomain.
-#if RELEASE
-            AppDomainSetup ads = new AppDomainSetup();
-            ads.ApplicationBase =
-                System.Environment.CurrentDirectory;
-            ads.DisallowBindingRedirects = false;
-            ads.DisallowCodeDownload = true;
-            ads.ConfigurationFile =
-                AppDomain.CurrentDomain.SetupInformation.ConfigurationFile;
-
-            // Create the second AppDomain.
-            AppDomain ad2 = AppDomain.CreateDomain("MV Domain", null, ads);
-
-            // Create an instance of MarshalbyRefType in the second AppDomain. 
-            // A proxy to the object is returned.
-            Startup startup =
-                (Startup)ad2.CreateInstanceAndUnwrap(
-                    Assembly.GetEntryAssembly().FullName,
-                    typeof(Startup).FullName
-                );
-
-            startup.RunProgram();
-
-            Console.WriteLine("Disposing of appdomain");
-            AppDomain.Unload(ad2);
-#else
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainWindow());
-#endif
 		}
-
-
-		/*		[STAThread]
-		static void Main()
-		{
-			//#if !DEBUG
-			//			try
-			//			{
-			//				Application.Run(new MainWindow());
-			//			}
-			//			catch(Exception e)
-			//			{
-			//				xConsole.AddLine("There was an error: "+e.Message+"\n"+e.StackTrace);
-			//				xConsole.AddLine("Type OK to close");
-			//				xConsole.Instance.ExternalCall+=new ExternalCallDelegate(myQuit);
-			//				Application.Run(xConsole.Instance);
-			//			}
-			//#else
-			try
-			{
-				Application.Run(new MainWindow());
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show("Unhandled exception, set the line logFile:true in your paths.pth and include the output from debug.log and this error message in your bug report. Thanks =)\n" + ex.Message + "\n" + ex.StackTrace);
-			}
-			//#endif
-		}*/
 	}
 }
