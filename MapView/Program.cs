320fb66ae169b5188544a531e63f1f44c6a744b1
Ben Ratzlaff <ben.ratzlaff@gmail.com>
2012-02-27 06:27:58 +0000

    Start new form with MapView
    git-svn-id: https://svn.code.sf.net/p/xcmapedit/code/trunk/dev@58 8cc3ff80-43d4-47b0-9ae9-63cba5b67482

--- MapView/Program.cs
+++ MapView/Program.cs
@@ -16,6 +16,7 @@ namespace MapView
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			//Application.Run(new MainWindow());
+			new MainWindow().Show();
 			Application.Run(new MainMapView());
 		}
 	}
