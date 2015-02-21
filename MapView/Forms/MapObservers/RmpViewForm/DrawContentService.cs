a11c8ea7299dba8bcb0d60080f9b59df0dd72864
TheBigSot <sot_c2@hotmail.com>
2015-02-21 04:34:26 -0400

    Added some ContentTypes

--- MapView/Forms/MapObservers/RmpViewForm/DrawContentService.cs
+++ MapView/Forms/MapObservers/RmpViewForm/DrawContentService.cs
@@ -33,71 +33,95 @@ namespace MapView.Forms.MapObservers.RmpViewForm
         {
             var contentType = _contentTypeService.GetContentType(content);
             var isDoor = _contentTypeService.IsDoor(content);
-            var toWallMargin = 4;
-            var topCorner = new Point(x, y + toWallMargin);
-            var bottomCorner = new Point(x, y + (HHeight * 2) - toWallMargin);
-            var leftCorner = new Point(x - HWidth + (toWallMargin * 2), y + HHeight);
-            var rightCorner = new Point(x + HWidth - (toWallMargin * 2), y + HHeight);
+            const int TO_WALL_MARGIN = 4;
+            var topCorner = new Point(x, y + TO_WALL_MARGIN);
+            var bottomCorner = new Point(x, y + (HHeight * 2) - TO_WALL_MARGIN);
+            var leftCorner = new Point(x - HWidth + (TO_WALL_MARGIN * 2), y + HHeight);
+            var rightCorner = new Point(x + HWidth - (TO_WALL_MARGIN * 2), y + HHeight);
 
-            if (contentType == ContentTypes.Content)
+            switch (contentType)
             {
-                var w = HWidth / 2;
-                var h = HHeight / 2;
-                y += h;
-                _content.Reset();
-                _content.AddLine(x, y, x + w, y + h);
-                _content.AddLine(x + w, y + h, x, y + h * 2);
-                _content.AddLine(x, y + h * 2, x - w, y + h);
-                _content.CloseFigure();
-                g.FillPath(color.Brush, _content);
-            }
-            else if (contentType == ContentTypes.NorthWall)
-            {
-                g.DrawLine(color.Pen, topCorner, rightCorner);
-                if (isDoor)
-                {
-                    g.DrawLine(color.Pen, topCorner, Point.Add(rightCorner, new Size(-10, 4)));
-                }
-            }
-            else if (contentType == ContentTypes.WestWall)
-            {
-                g.DrawLine(color.Pen, topCorner, leftCorner);
-                if (isDoor)
-                {
-                    g.DrawLine(color.Pen, Point.Add(topCorner, new Size(6, 8)), leftCorner);
-                }
-            }
-            else if (contentType == ContentTypes.NorthWallWithWindow)
-            {
-                DrawWindow(g, color, topCorner, rightCorner);
-            }
-            else if (contentType == ContentTypes.WestWallWithWindow)
-            {
-                DrawWindow(g, color, topCorner, leftCorner);
-            }
-            else if (contentType == ContentTypes.SouthWall)
-            {
-                g.DrawLine(color.Pen, leftCorner, bottomCorner);
-            }
-            else if (contentType == ContentTypes.EastWall)
-            {
-                g.DrawLine(color.Pen, bottomCorner, rightCorner);
-            }
-            else if (contentType == ContentTypes.NW_To_SE)
-            {
-                g.DrawLine(color.Pen, topCorner, bottomCorner);
-            }
-            else if (contentType == ContentTypes.NorthWestCorner)
-            {
-                g.DrawLine(color.Pen,
-                    Point.Add(topCorner, new Size(-4, 0)),
-                    Point.Add(topCorner, new Size(4, 0)));
-            }
-            else if (contentType == ContentTypes.NE_To_SW)
-            {
-                g.DrawLine(color.Pen, leftCorner, rightCorner);
+                case ContentTypes.Content:
+                    SetGroundPath(x, y);
+                    g.FillPath(color.Brush, _content);
+                    break;
+                case ContentTypes.Ground:
+                    SetGroundPath(x, y);
+                    g.FillPath(color.LightBrush, _content);
+                    break;
+                case ContentTypes.NorthFence:
+                    g.DrawLine(color.LightPen, topCorner, rightCorner);
+                    break;
+                case ContentTypes.NorthWall:
+                    g.DrawLine(color.Pen, topCorner, rightCorner);
+                    if (isDoor)
+                    {
+                        g.DrawLine(color.Pen, topCorner, Point.Add(rightCorner, new Size(-10, 4)));
+                    }
+                    break;
+                case ContentTypes.WestFence:
+                    g.DrawLine(color.LightPen, topCorner, leftCorner);
+                    break;
+                case ContentTypes.WestWall:
+                    g.DrawLine(color.Pen, topCorner, leftCorner);
+                    if (isDoor)
+                    {
+                        g.DrawLine(color.Pen, Point.Add(topCorner, new Size(6, 8)), leftCorner);
+                    }
+                    break;
+                case ContentTypes.NorthWallWithWindow:
+                    DrawWindow(g, color, topCorner, rightCorner);
+                    break;
+                case ContentTypes.WestWallWithWindow:
+                    DrawWindow(g, color, topCorner, leftCorner);
+                    break;
+                case ContentTypes.SouthWall:
+                    g.DrawLine(color.Pen, leftCorner, bottomCorner);
+                    break;
+                case ContentTypes.EastWall:
+                    g.DrawLine(color.Pen, bottomCorner, rightCorner);
+                    break;
+                case ContentTypes.NW_To_SE:
+                    g.DrawLine(color.Pen, topCorner, bottomCorner);
+                    break;
+                case ContentTypes.NE_To_SW:
+                    g.DrawLine(color.Pen, leftCorner, rightCorner);
+                    break;
+
+                case ContentTypes.NorthWestCorner:
+                    g.DrawLine(color.Pen,
+                        Point.Add(topCorner, new Size(-4, 0)),
+                        Point.Add(topCorner, new Size(4, 0)));
+                    break;
+                case ContentTypes.NorthEastCorner:
+                    g.DrawLine(color.Pen,
+                        Point.Add(rightCorner, new Size(0, -4)),
+                        Point.Add(rightCorner, new Size(0, 4)));
+                    break;
+                case ContentTypes.SouthEastCorner:
+                    g.DrawLine(color.Pen,
+                        Point.Add(bottomCorner, new Size(-4, 0)),
+                        Point.Add(bottomCorner, new Size(4, 0)));
+                    break;
+                case ContentTypes.SouthWestCorner:
+                    g.DrawLine(color.Pen,
+                        Point.Add(leftCorner, new Size(0, -4)),
+                        Point.Add(leftCorner, new Size(0, 4)));
+                    break;
             }
 
+        }
+
+        private void SetGroundPath(int x, int y)
+        {
+            var w = HWidth / 2;
+            var h = HHeight / 2;
+            y += h;
+            _content.Reset();
+            _content.AddLine(x, y, x + w, y + h);
+            _content.AddLine(x + w, y + h, x, y + h * 2);
+            _content.AddLine(x, y + h * 2, x - w, y + h);
+            _content.CloseFigure();
         }
 
         private static void DrawWindow(Graphics g, SolidPenBrush color, Point start, Point end)
@@ -106,10 +130,7 @@ namespace MapView.Forms.MapObservers.RmpViewForm
             var size = new Size(dist.X / 3, dist.Y / 3);
             var point2 = Point.Add(start, Size.Add(size, size));
             g.DrawLine(color.Pen, start, Point.Add(start, size));
-            var c = color.Pen.Color;
-            color.Pen.Color = Color.FromArgb(50, c);
-            g.DrawLine(color.Pen, Point.Add(start, size), point2);
-            color.Pen.Color = c;
+            g.DrawLine(color.LightPen, Point.Add(start, size), point2);
             g.DrawLine(color.Pen, point2, end);
         }
 
--- MapView/Forms/MapObservers/TopViewForm/ContentTypeService.cs
+++ MapView/Forms/MapObservers/TopViewForm/ContentTypeService.cs
@@ -13,17 +13,31 @@ namespace MapView.Forms.MapObservers.TopViewForm
             var mcdEntry = content.Info as McdEntry;
             if (mcdEntry == null) return ContentTypes.Content;
             var loftList = mcdEntry.GetLoftList();
+            var allButGround = new List<byte>(loftList);
+            allButGround.RemoveAt(0);
+            if (AllLoftWith(allButGround, new[] {0})) return ContentTypes.Ground;
             if (AllLoftWith(loftList, new[] {24, 26})) return ContentTypes.EastWall;
             if (AllLoftWith(loftList, new[] {23, 25})) return ContentTypes.SouthWall;
+
             if (AllLoftWith(loftList, new[] {8, 10, 12, 14, 38}) &&
                 HasAnyLoftWith(loftList, new[] {38})) return ContentTypes.NorthWallWithWindow;
+            if (AllLoftWith(loftList, new[] { 8, 10, 12, 14, 38, 0, 39, 77 }) &&
+                HasAnyLoftWith(loftList, new[] {0})) return ContentTypes.NorthFence;
+            if (AllLoftWith(loftList, new[] {8, 10, 12, 14})) return ContentTypes.NorthWall;
+
             if (AllLoftWith(loftList, new[] {7, 9, 11, 13, 37}) &&
                 HasAnyLoftWith(loftList, new[] {37})) return ContentTypes.WestWallWithWindow;
-            if (AllLoftWith(loftList, new[] {8, 10, 12, 14})) return ContentTypes.NorthWall;
+            if (AllLoftWith(loftList, new[] { 7, 9, 11, 13, 37,0, 39, 76 }) &&
+                HasAnyLoftWith(loftList, new[] {0})) return ContentTypes.WestFence;
             if (AllLoftWith(loftList, new[] {7, 9, 11, 13})) return ContentTypes.WestWall;
+
             if (AllLoftWith(loftList, new[] {35})) return ContentTypes.NW_To_SE;
             if (AllLoftWith(loftList, new[] {36})) return ContentTypes.NE_To_SW;
-            if (AllLoftWith(loftList, new[] {39, 40, 41, 103})) return ContentTypes.NorthWestCorner;
+
+            if (AllLoftWith(loftList, new[] { 39, 40, 41, 103 })) return ContentTypes.NorthWestCorner;
+            if (AllLoftWith(loftList, new[] { 100 })) return ContentTypes.NorthEastCorner;
+            if (AllLoftWith(loftList, new[] { 106 })) return ContentTypes.SouthWestCorner;
+            if (AllLoftWith(loftList, new[] { 109 })) return ContentTypes.SouthEastCorner;
             return ContentTypes.Content;
         }
 
@@ -81,6 +95,12 @@ namespace MapView.Forms.MapObservers.TopViewForm
         NE_To_SW,
         NorthWallWithWindow,
         WestWallWithWindow,
-        NorthWestCorner
+        Ground,
+        NorthFence,
+        WestFence,
+        NorthWestCorner,
+        NorthEastCorner,
+        SouthWestCorner,
+        SouthEastCorner
     }
 }
